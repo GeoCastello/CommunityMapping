201c569d8035cd7464010f57201a6cb55cdfe571
AmyWootton1995 <u14074657@tuks.co.za>
2017-08-03 14:27:12 +0200

    Satellite image

--- www/js/app.js
+++ www/js/app.js
@@ -34,10 +34,10 @@ var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-m
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
-var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-   maxZoom: 20,
-     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    });
+var Esri_WorldImagery = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
 Esri_WorldImagery.addTo(map)
 
 var Social = L.geoJson();
